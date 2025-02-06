55e0e3c6468a998dee3ee6c4697b702071d4cfe9
priyanshu-1234444 <153397191+priyanshu-1234444@users.noreply.github.com>
2025-02-06 11:42:27 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  fontFamily: {
+    body: ["Fjalla One"],
+    'sans': ["Poppins",'Roboto', 'sans-serif'],
+    
+  }
+}
